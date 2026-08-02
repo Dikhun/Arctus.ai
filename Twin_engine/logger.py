4cd86e5e91e90241869cb31d8346148d130809e1
Dinesh khuman <dineshkhuman8014@gmail.com>
2026-08-02 07:21:26 +0530

    Implement logging configuration in logger.py
    Add a logging configuration using structlog and RichHandler.
    
    Signed-off-by: Dinesh khuman <dineshkhuman8014@gmail.com>

---
+++ Twin_engine/logger.py
@@ -0,0 +1,28 @@
+import logging
+import structlog
+from rich.console import Console
+from rich.logging import RichHandler
+
+def configure_logging(log_level: str = "INFO") -> None:
+    structlog.configure(
+        processors=[
+            structlog.stdlib.filter_by_level,
+            structlog.stdlib.add_logger_name,
+            structlog.stdlib.add_log_level,
+            structlog.stdlib.PositionalArgumentsFormatter(),
+            structlog.processors.TimeStamper(fmt="iso"),
+            structlog.processors.StackInfoRenderer(),
+            structlog.processors.format_exc_info,
+            structlog.processors.UnicodeDecoder(),
+            structlog.dev.ConsoleRenderer(colors=True),
+        ],
+        context_class=dict,
+        logger_factory=structlog.stdlib.LoggerFactory(),
+        wrapper_class=structlog.stdlib.BoundLogger,
+        cache_logger_on_first_use=True,
+    )
+    logging.basicConfig(
+        format="%(message)s",
+        level=log_level,
+        handlers=[RichHandler(console=Console(stderr=True), rich_tracebacks=True, show_path=False)],
+    )
